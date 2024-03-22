a2fd07942aab6e9eeff2c4cf11956747511ad762
The _IsaacAgboola <isaacadeyemi9@gmail.com>
2024-03-22 16:14:46 +0100

    Authorization-done, Access Token -gotten

--- script.js
+++ script.js
@@ -32,52 +32,101 @@ colorModeButton.addEventListener("click", () => {
 const clientID = "1d7a3c5bd8f44d82896028c754d7898a";
 const clientSecret = "b1d908257adc4865be1f88b343bc58f7";
 const tokenEndpoint = "https://accounts.spotify.com/api/token";
-
-let accessToken =
-  "BQB8oQO_F1mgVlNbBZIj-3kq7YxOYUObuc5pk4Wq7nCYreZSyFxSaGtWF7dF8MUwYM_wdYsEkr2PWwskBQpUuFd4GpMPzywFTsFS5NBIpnjAz9_28HI";
-
-async function getAccessToken() {
-  try {
-    const response = await fetch(tokenEndpoint, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: `grant_type=client_credentials&client_id=${clientID}&client_secret=${clientSecret}`,
-    });
-
-    const { access_token } = await response.json();
-    return access_token;
-  } catch (err) {
-    console.log(err);
-  }
-}
-
-// const myToken = await getAccessToken();
-// console.log(myToken);
+const redirectLocation = "http://127.0.0.1:5500/index.html";
 
 // Authorization
+async function getAuthorization() {
+  //step1
+  const generateRandomString = (length) => {
+    const possible =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+  };
 
-function getUserData(token) {
-  const userDataEndpoint = "https://api.spotify.com/v1/me";
+  const codeVerifier = generateRandomString(64);
 
-  fetch(userDataEndpoint, {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  })
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  //step2
+  const sha256 = async (plain) => {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(plain);
+    return window.crypto.subtle.digest("SHA-256", data);
+  };
+
+  //step3
+  const base64encode = (input) => {
+    return btoa(String.fromCharCode(...new Uint8Array(input)))
+      .replace(/=/g, "")
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_");
+  };
+
+  //step4
+  const hashed = await sha256(codeVerifier);
+  const codeChallenge = base64encode(hashed);
+
+  // ACTUAL IMPLEMENTATTION
+  const clientId = clientID;
+  const redirectUri = redirectLocation;
+
+  const scope =
+    "user-read-private user-read-email streaming playlist-read-private user-follow-read user-top-read";
+  const authUrl = new URL("https://accounts.spotify.com/authorize");
+
+  // generated in the previous step
+  window.localStorage.setItem("code_verifier", codeVerifier);
+
+  const params = {
+    response_type: "code",
+    client_id: clientId,
+    scope,
+    code_challenge_method: "S256",
+    code_challenge: codeChallenge,
+    redirect_uri: redirectUri,
+  };
+
+  authUrl.search = new URLSearchParams(params).toString();
+  window.location.href = authUrl.toString();
 }
 
-getUserData(accessToken);
+// INVOKE SPOTIFY REDIRECTION
 
-// async function fetchProfile(token) {
-//   const result = await fetch("https://api.spotify.com/v1/me", {
-//     method: "GET",
-//     headers: { Authorization: `Bearer ${token}` },
-//   });
+document.getElementById("connect").addEventListener("click", () => {
+  getAuthorization();
+});
 
-//   return await result.json();
-// }
+const urlParams = new URLSearchParams(window.location.search);
+let code = urlParams.get("code");
+
+// console.log(urlParams);
+// console.log(code);
+
+// We then get an Access Token
+async function getToken(code) {
+  // stored in the previous step
+  let codeVerifier = localStorage.getItem("code_verifier");
+
+  const message = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: new URLSearchParams({
+      client_id: clientID,
+      grant_type: "authorization_code",
+      code,
+      redirect_uri: redirectLocation, //redirectUri
+      code_verifier: codeVerifier,
+    }),
+  };
+
+  const body = await fetch(tokenEndpoint, message);
+  const response = await body.json();
+  console.log(response);
+
+  localStorage.setItem("access_token", response.access_token);
+  localStorage.setItem("refresh_token", response.refresh_token);
+}
+
+getToken(code);
+// console.log(localStorage.getItem("access_token"));    works!!!
